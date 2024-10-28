3a0b4e4df056adcc97899703e737cd05331727b4
Morphi Doleo <morphidoleo07@gmail.com>
2024-10-28 16:38:40 -0400

    *estructura inicial del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import PaginaProductos from './components/PaginaProductos';
+
+function App() {
+  return (
+    <>
+      <PaginaProductos />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/PaginaProductos.jsx
@@ -0,0 +1,11 @@
+// src/paginas/PaginaProductos.js
+
+const PaginaProductos = () => {
+  return (
+    <div>
+      <h2>Inicio</h2>
+    </div>
+  );
+};
+
+export default PaginaProductos;
